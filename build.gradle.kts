e92b06416cdd67aea20072562c8e946625b39b23
Santiago Vélez Yepes <svy9602@hotmail.com>
2026-09-25 13:21:46 -0500

    se actualiza runner para ver las evidencias de serenity bdd

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     java
     idea
+    // 1. Plugin oficial para compilar el reporte visual "bonito" de Serenity
+    id("net.serenity-bdd.serenity-gradle-plugin") version "4.2.26"
 }
 
 group = "com.qvision.bonbonite"
@@ -10,33 +12,37 @@ repositories {
     mavenCentral()
 }
 
+val serenityVersion = "4.2.26"
+
 dependencies {
-    // Serenity BDD
-    testImplementation("net.serenity-bdd:serenity-core:4.2.0")
-    testImplementation("net.serenity-bdd:serenity-screenplay:4.2.0")
-    testImplementation("net.serenity-bdd:serenity-screenplay-webdriver:4.2.0")
-    testImplementation("net.serenity-bdd:serenity-cucumber:4.2.0")
-    testImplementation("net.serenity-bdd:serenity-ensure:4.2.0")
-    
-    // Cucumber
+    // Serenity BDD Core
+    testImplementation("net.serenity-bdd:serenity-core:$serenityVersion")
+    testImplementation("net.serenity-bdd:serenity-screenplay:$serenityVersion")
+    testImplementation("net.serenity-bdd:serenity-screenplay-webdriver:$serenityVersion")
+    testImplementation("net.serenity-bdd:serenity-cucumber:$serenityVersion")
+    testImplementation("net.serenity-bdd:serenity-ensure:$serenityVersion")
+
+    // Cucumber + JUnit 5 Platform
     testImplementation("io.cucumber:cucumber-java:7.18.0")
     testImplementation("io.cucumber:cucumber-junit-platform-engine:7.18.0")
-    
+
     // JUnit 5
     testImplementation("org.junit.platform:junit-platform-suite:1.11.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
-    
+
     // AssertJ
-    testImplementation("org.assertj:assertj-core:3.26.0")
+    testImplementation("org.assertj:assertj-core:3.26.3")
 }
 
 tasks.test {
     useJUnitPlatform()
+
     systemProperty("cucumber.junit-platform.naming-strategy", "long")
+
+    // 2. Hace que al terminar 'test', se ejecute automáticamente el reporte de Serenity
+    finalizedBy("aggregate")
 }
 
-// Serenity configuration
 tasks.withType<Test> {
     systemProperty("serenity.project.name", "Bon-Bonite Automation")
-    systemProperty("serenity.outputDirectory", "${project.buildDir}/serenity")
 }
--- src/test/java/com/qvision/bonbonite/runners/registro/RegistroTestSuite.java
+++ src/test/java/com/qvision/bonbonite/runners/registro/RegistroTestSuite.java
@@ -5,11 +5,14 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = "cucumber.glue", value = "com.qvision.bonbonite.stepdefinitions,net.serenitybdd.cucumber.actors")
 @ConfigurationParameter(key = "cucumber.plugin", value = "pretty")
 @ConfigurationParameter(key = "cucumber.filter.tags", value = "@registro")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel")
 public class RegistroTestSuite {
 }
